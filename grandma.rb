e671c35bd8d320cee7a22b9f4f2911f5ef2c7cde
Yuki N <lunarbase@outlook.com>
2018-10-27 21:47:35 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.downcase
+  if phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
